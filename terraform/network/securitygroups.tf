c46e7bd7ab4b4acb2687234a71d42ac25dd91672
rpaquito-unit <ricardo.paquito@outsystems.com>
2022-06-22 14:13:03 +0100

    Add self rules to private sg

--- terraform/network/securitygroups.tf
+++ terraform/network/securitygroups.tf
@@ -66,6 +66,24 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
+resource "aws_security_group_rule" "private_sg_self_80" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  self              = true
+  security_group_id = aws_security_group.private_sg.id
+}
+
+resource "aws_security_group_rule" "private_sg_self_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  self              = true
+  security_group_id = aws_security_group.private_sg.id
+}
+
 # Create Database Security Group
 resource "aws_security_group" "database_sg" {
   name        = "${var.deploy_name}-db-sg"
